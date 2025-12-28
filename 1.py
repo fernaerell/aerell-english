cae329c66586fd06c2086b7a89ca8d6e4b4f6059
Fern Aerell <fernaerell2020@gmail.com>
2025-12-28 07:45:59 +0700

    chore: Day 1 learn English with python

---
+++ 1.py
@@ -0,0 +1,48 @@
+# Learn English day 1 using python
+
+# I / You / We
+# Is / are / do
+# Focus verb: print, get, set, check
+
+# I print text Hello World to the console
+print("Hello World")
+
+# I set a variable name with my name
+name = "Fern Aerell"
+
+# I print my name in variable like printed hello world before
+print(name)
+
+# You can get my name in variable name like this
+name # but it is not displaying in console, why? because the expression not in print function.
+
+# You must put the variable into function print argument, like this:
+print(name)
+
+# We have printed my name 2 times, you can see in console.
+
+# We can put my name in another variable
+name2 = name
+
+# Now name2 is my name too, we check:
+print(name2)
+
+# If you feel this less clarity, you can add some string before argument name2 in print function, but don't forget about coma , for sparator
+print("this is from name 2 variable:", name2)
+
+# We check again, and see the different.
+
+# Now we have printed my name  4 times.
+
+# Let's do something different
+# We add name to with name2 as print function argument
+print(name + name2)
+# Let see!
+
+# I think on python we can do this:
+print(name*10)
+# Haha it's cool
+# I found this feature just in python, i don't know about another language, but I first found this feature in python.
+
+# Okay I thing this enough.
+# Thank you.
