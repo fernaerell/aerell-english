7abee53f584fa09901774df40a1b1dde04b3b187
Fern Aerell <fernaerell2020@gmail.com>
2025-12-28 07:55:46 +0700

    refactor: Add the output in terminal in 1.py

--- 1.py
+++ 1.py
@@ -6,18 +6,27 @@
 
 # I print text Hello World to the console
 print("Hello World")
+# the output like this:
+# Hello World
 
 # I set a variable name with my name
 name = "Fern Aerell"
 
 # I print my name in variable like printed hello world before
 print(name)
+# Now the output like this:
+# Hello World
+# Fern Aerell
 
 # You can get my name in variable name like this
 name # but it is not displaying in console, why? because the expression not in print function.
 
 # You must put the variable into function print argument, like this:
 print(name)
+# The output updated like this:
+# Hello World
+# Fern Aerell
+# Fern Aerell
 
 # We have printed my name 2 times, you can see in console.
 
@@ -26,9 +35,20 @@ name2 = name
 
 # Now name2 is my name too, we check:
 print(name2)
+# The output:
+# Hello World
+# Fern Aerell
+# Fern Aerell
+# Fern Aerell
 
 # If you feel this less clarity, you can add some string before argument name2 in print function, but don't forget about coma , for sparator
 print("this is from name 2 variable:", name2)
+# The output like this now:
+# Hello World
+# Fern Aerell
+# Fern Aerell
+# Fern Aerell
+# this is from name 2 variable: Fern Aerell
 
 # We check again, and see the different.
 
@@ -38,9 +58,25 @@ print("this is from name 2 variable:", name2)
 # We add name to with name2 as print function argument
 print(name + name2)
 # Let see!
+# The output like this:
+# Hello World
+# Fern Aerell
+# Fern Aerell
+# Fern Aerell
+# this is from name 2 variable: Fern Aerell
+# Fern AerellFern Aerell
 
 # I think on python we can do this:
 print(name*10)
+# The output:
+# Hello World
+# Fern Aerell
+# Fern Aerell
+# Fern Aerell
+# this is from name 2 variable: Fern Aerell
+# Fern AerellFern Aerell
+# Fern AerellFern AerellFern AerellFern AerellFern AerellFern AerellFern AerellFern AerellFern AerellFern Aerell
+
 # Haha it's cool
 # I found this feature just in python, i don't know about another language, but I first found this feature in python.
 
